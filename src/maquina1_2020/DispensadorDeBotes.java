9d547308095220acd32885ba1d4d87df4a6142cf
Jenifer Benavent <jbenavent@iesfuentesanluis.org>
2021-04-29 13:56:52 +0200

    "Jeni commit"

---
+++ src/maquina1_2020/DispensadorDeBotes.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package maquina1_2020;
+import java.util.*;
+/**
+ *
+ * @author jenif
+ */
+public class DispensadorDeBotes {
+    
+    Scanner tec=new Scanner (System.in);
+
+    private int botes; //Numero actual de botes.
+
+    
+    public DispensadorDeBotes (int i) {
+
+        this.botes=i;
+
+    }
+
+    public boolean pulsarBoton () {
+            boolean hayBotes=false;
+
+            if (botesDisponibles()>0) {
+        hayBotes= true;
+        botes--;
+
+            }
+            return hayBotes;
+
+    }
+
+    
+    public int botesDisponibles () {
+        int botesDisp=botes;
+        System.out.println("---Num de botes disponibles---");
+        return botesDisp;
+
+        }
+
+}
