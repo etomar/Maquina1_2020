dced8846b35b7f38f31266472e684b448b76c297
JFabregat3 <jfabregat3@iesfuentesanluis.org>
2021-05-06 16:43:54 +0200

    javi commit javadoc

--- src/maquina1_2020/ContadorDeMonedas.java
+++ src/maquina1_2020/ContadorDeMonedas.java
@@ -1,36 +1,62 @@
 package maquina1_2020;
 
+/**
+ * 
+ * @author javier Fabregat
+ * @version 1.0
+ */
 public class ContadorDeMonedas {
-	private float cantidad;
-	private float disponible;
-	
-	public ContadorDeMonedas(float i) {
-		this.disponible=i;
-	}
-	
-	public float saldo() {
-		float monedas=cantidad;
-		return monedas;
-	}
-	
-	public void insertarMoneda(float m) {
-		this.cantidad+=m;
-	}
-	
-	public float darCambioDe(float c) {
-		float cambio=cantidad-c;
-		if (cambio>=0){
-			disponible+=cantidad;
-			cantidad=0;
-		}
-		return cambio;
-	}
-	
-	public float retornar() {
-		float devolucion=cantidad;
-		if (devolucion>0)
-			cantidad=0;
-		return devolucion;
-	}
+    private float cantidad;
+    private float disponible;
+    
+        /**
+         * 
+         * @param i Dinero disponible de la maquina
+         */
+    public ContadorDeMonedas(float i) {
+        this.disponible=i;
+    }
+        
+    /**
+         * 
+         * @return Devuelve la cantidad de monedas introducidas
+         */
+    public float saldo() {
+        float monedas=cantidad;
+        return monedas;
+    }
+    
+        /**
+         * 
+         * @param m Cantidad total de monedas introducidas
+         */
+    public void insertarMoneda(float m) {
+        this.cantidad+=m;
+    }
+    
+        /**
+         * 
+         * @param c Cantidad a restar de la máquina
+         * @return Devuelve el cambio de la transaccion
+         */
+    public float darCambioDe(float c) {
+        float cambio=cantidad-c;
+        if (cambio>=0){
+            disponible+=cantidad;
+            cantidad=0;
+        }
+        return cambio;
+    }
+    
+        /**
+         * 
+         * @return Devueve el dinero introdicido si no se ha realizado la transacción
+         */
+    public float retornar() {
+        float devolucion=cantidad;
+        if (devolucion>0)
+            cantidad=0;
+        return devolucion;
+    }
 
 }
